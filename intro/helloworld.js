48d84db53d73e4e20b5306663c6705686a2334bc
DavidCastillo4 <David.Castillo4@gmail.com>
2020-06-01 18:13:38 -0500

    completed solutions

--- intro/helloworld.js
+++ intro/helloworld.js
@@ -1,6 +1,55 @@
 'use strict'
-console.log('Hellow World');
-let x=7;
-let y=8;
-let z=x+y;
-console.log('The value of z is:' + z)
+
+//#1
+let DateString=Date()
+let DateObject=new Date()
+console.log('#1 DateString: '+DateString)
+console.log('#1 DateObject: '+DateObject)
+
+let i = 123;
+console.log('#2 Convert number to string: '+i.toString())
+
+let n = '123';
+console.log('#3 Convert string to number: '+parseInt(n))
+
+
+let myBool=true,myUndefined,myNum=123,myString='abc'
+let arr=[myBool,myUndefined,myNum,myString]
+
+arr.forEach(function(val,i){   
+  console.log('#4 '+typeof val)  
+});
+
+let Add2numbers=(a,b)=>{
+  console.log(`#5 Add two numbers: ${a}+${b}=${a+b}`)
+}
+
+Add2numbers(2,3)
+
+let RunIfBothTrue=(a,b)=>{
+  if (a==true && b==true)
+  {console.log(`#6 RunIfBothTrue: ${a} and ${b} are both true`)}
+}
+
+RunIfBothTrue(1,1)
+
+let RunIfEitherTrue=(a,b)=>{
+  if (a==true || b==true)
+  {console.log(`#7 RunIfEitherTrue: either ${a} or ${b} is true`)}
+}
+
+RunIfEitherTrue(1,)
+
+let RunIfBothFalse=(a,b)=>{
+  if (a==false && b==false)
+  {console.log(`#8 RunIfBothFalse: both ${a} and ${b} are false`)}
+}
+
+RunIfBothFalse(false,false)
+
+
+
+
+
+
+
